837a6693623c0f4a02f369cb5b910bedff14b661
Khouloud <khouloudzayati@gmail.com>
2019-01-11 15:45:57 +0100

    test1

--- app/src/main/java/com/example/khouloud/scpv2/utils/Connexion.java
+++ app/src/main/java/com/example/khouloud/scpv2/utils/Connexion.java
@@ -2,7 +2,7 @@ package com.example.khouloud.scpv2.utils;
 
 public class Connexion {
 
-    public final static String url ="http://localhost:8085/TestWebService/services/YWEBSERV?wsdl";
+    public final static String url ="http://192.168.1.141:8085/TestWebService/services/YWEBSERV?wsdl";
 
 
 }
